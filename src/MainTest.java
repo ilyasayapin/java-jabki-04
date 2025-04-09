35daf9d90e0c1864062b29e6d4d41069f9c61f31
ilyasayapin <kite@nxt.ru>
2025-04-09 23:11:04 +0300

    Lesson 4. Homework

---
+++ src/MainTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void sumRange() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findMax() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateFactorial() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testCalculateArea() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateAverage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateHypotenuse() {
+    }
+}
