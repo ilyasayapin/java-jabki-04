2d8322cbf834736b228212fea7e314d6ba051dc9
ilyasayapin <kite@nxt.ru>
2025-04-09 23:33:38 +0300

    Lesson 4. Homework

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,13 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @org.junit.jupiter.api.Test
     void sumRange() {
+        int a = 7;
+        int b = 9;
+        Assertions.assertEquals(24, Main.sumRange(a, b));
     }
 
     @org.junit.jupiter.api.Test
-    void add() {
+    void addFirst() {
+        int a = 1;
+        int b = 9;
+        Assertions.assertEquals(10, Main.add(a, b));
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSecond() {
+        String a = "Добрый ";
+        String b = "день!";
+        Assertions.assertEquals("Добрый день!", Main.add(a, b));
     }
 
     @org.junit.jupiter.api.Test
